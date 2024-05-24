bb41dfd3be39be0c246e0c9341f779d49fd700fd
Hoang Thien Dang Nguyen <159185239+dang-2nguyen@users.noreply.github.com>
2024-05-24 18:12:16 +0000

    sdfwe

--- js/script.js
+++ js/script.js
@@ -1,5 +1,24 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2024 Dang All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Dang
+// Created on: May 2024
 // This file contains the JS functions for index.html
+
+"use strict"
+
+window.onload = function () {
+  // this calculates volume of a pyramid
+
+  const params = new URLSearchParams(document.location.search)
+
+  // input
+  const radiusOfASphere = params.get('radiusOfASphere')
+
+  // process
+  const volume = 4/3 * Math.PI * radiusOfASphere
+  const dimensions = "<ul>\n<li>radius of a sphere = " + radiusOfASphere
+
+  // output
+  document.getElementById('dimensions').innerHTML = dimensions
+  document.getElementById('volume').innerHTML = 'volume is: ' + volume.toFixed(2)
+}
